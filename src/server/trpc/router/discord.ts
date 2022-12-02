cc4ff5328ab8179fb3fcca10c486f0e7b77ae971
robert davidson <robertjohnd@live.ca>
2022-12-01 20:12:26 -0500

    Add Deno Discord bot to repo
    will be separately deployed, just in here for cohesion

--- src/server/trpc/router/discord.ts
+++ src/server/trpc/router/discord.ts
@@ -3,24 +3,6 @@ import { router, protectedProcedure } from "../trpc";
 import { env } from "../../../env/server.mjs";
 
 export const discord = router({
-  getGuilds: protectedProcedure
-    .input(
-      z.object({
-        accessToken: z.string().nullish(),
-      })
-    )
-    .query(async ({ input }) => {
-      return fetch(`${env.DISCORD_API_URL}/users/@me/guilds`, {
-        headers: { Authorization: `Bearer ${input.accessToken}` },
-      })
-        .then((res) => {
-          console.log(res);
-          return res.json();
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }),
   checkBotMembership: protectedProcedure
     .input(
       z.object({
@@ -28,7 +10,7 @@ export const discord = router({
         memberGuilds: z.array(z.object({ id: z.string() })),
       })
     )
-    .query(async ({ input }) => {
+    .query(({ input }) => {
       for (const guild of input.memberGuilds) {
         fetch(
           `${env.DISCORD_API_URL}/guilds/${guild.id}/members/${env.DISCORD_BOT_ID}`,
